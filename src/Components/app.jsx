5d9981e3e0b197d1bf7ba3eb382e5fb7493292f6
spencerjamesdev <spencer.franke@gmail.com>
2021-08-13 16:47:25 -0500

    Added console log to App.jsx, now it displays songs on console

--- src/Components/app.jsx
+++ src/Components/app.jsx
@@ -1,14 +1,25 @@
 import { render } from '@testing-library/react';
 import { Component} from 'react';
+import axios from 'axios';
 
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = {
+            data: []
+        };
+
     }
+
+    componentDidMount(){
+        axios.get("http://127.0.0.1:8000/music/").then(function(response){
+            console.log(response.data)
+        })
+    }
+
     render(){
         return(
-            <h1> testing</h1>
+            <h1>Testing</h1>
         )
 
     }
