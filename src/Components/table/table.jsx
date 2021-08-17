1334188ac666dff3f1d1859ba7261fa3a515ddeb
spencerjamesdev <spencer.franke@gmail.com>
2021-08-17 15:53:12 -0500

    finished delete button

--- src/Components/table/table.jsx
+++ src/Components/table/table.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import './table.css';
 import '../app';
-import axios from 'axios';
 
-const deleteSong = (songID) => {
-  axios.delete("http://127.0.0.1:8000/music/"+songID+"/")
-    .then(response => {
-      if(response.data != null) {
-        alert("Song deleted successfully.");
-      }
-    });
-};
+
+
 
 const MusicDisplayTable = (props) => {
     return ( 
@@ -30,7 +23,7 @@ const MusicDisplayTable = (props) => {
               <td>{data.artist}</td>
               <td>{data.album}</td>
               <td>{data.release_date}</td>
-              <button onClick={() => deleteSong(data.id)}>Delete</button>
+              <button className="button1" onClick={() => props.delete(data.id)}>Delete</button>
           </tr>))}
         </tbody>
       </table>
